b9098b1fe0fb6046c9ac932a0144957920a3073a
Rainier <rainiertabing21@gmail.com>
2025-10-31 22:34:34 -0400

    Added a getExit method and reduced repetitions in the goRoom method

--- Room.java
+++ Room.java
@@ -14,12 +14,29 @@
  */
 public class Room 
 {
-    public String description;
-    public Room northExit;
-    public Room southExit;
-    public Room eastExit;
-    public Room westExit;
-
+    private String description;
+    private Room northExit;
+    private Room southExit;
+    private Room eastExit;
+    private Room westExit;
+    
+    public Room getExit(String direction)
+    {
+        if(direction.equals("north")){
+            return northExit;
+        }
+        if(direction.equals("east")){
+            return eastExit;
+        }
+        if(direction.equals("south")){
+            return southExit;
+        }
+        if(direction.equals("west")){
+            return westExit;
+        }
+        return null;
+    }
+    
     /**
      * Create a room described "description". Initially, it has no exits. 
      * "description" is something like "a kitchen" or "an open court yard".
@@ -61,4 +78,5 @@ public class Room
     {
         return description;
     }
+    
 }
